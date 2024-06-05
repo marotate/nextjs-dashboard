655f9963b1b84e42fe8903d516b5e015f63cae60
ชุติกาญจน์ จองวิจิตรกุล <139462014+marotate@users.noreply.github.com>
2024-06-05 17:01:13 +0700

    search

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -4,16 +4,16 @@ import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
 import { useSearchParams, usePathname, useRouter } from 'next/navigation';
 import { useDebouncedCallback } from 'use-debounce';
 
-export default function Search({ placeholder}: {placeholder: string}) {
+export default function Search({placeholder}: {placeholder: string}) {
   const searchParams = useSearchParams();
   const pathname = usePathname();
   const { replace } = useRouter();
 
-  function handleSearch(term: string) {
   const handleSearch = useDebouncedCallback((term) => {
     console.log(`Searching... ${term}`);
 
     const params = new URLSearchParams(searchParams);
+    params.set('page', '1'); //reset the page to become 1 when user types a new query
     if (term) {
       params.set('query', term);
     } else {
@@ -38,4 +38,4 @@ export default function Search({ placeholder}: {placeholder: string}) {
       <MagnifyingGlassIcon className="absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
     </div>
   );
-}}
+}
